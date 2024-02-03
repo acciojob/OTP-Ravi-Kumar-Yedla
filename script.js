8db0b238545bd8bf47f64aaeef59ccd91a5a6efd
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-03 11:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function handleInput(input,event){
+	// automatically move to the next input on typing a number
+	if(/^\d$/.test(input.value) && input.nextElementSibling){
+		input.nextElementSibling.focus();
+	}
+}
+
+function handleKeyDown(input,event){
+	 // Move to the previous input on backspace key press
+	if(event.key ==="Backspace" && input.previousElementSibling){
+		input.previousElementSibling.focus();
+	}
+}
